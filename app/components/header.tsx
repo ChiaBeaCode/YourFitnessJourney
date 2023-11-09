145f1191be68a7136a3fe0a01abb9570e7a8d5aa
Ashley Favello <ashfavello@gmail.com>
2023-11-08 22:52:38 -0800

    added github link and some css

---
+++ app/components/header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+      <nav className=" flex justify-center text-center space-x-10 bg-blue opacity-90 bg-slate-300 h-20 border-2 border-b-slate-400">
+        <p className="text-center text-3xl m-auto pb-3">Your Fitness Journey</p>
+      </nav>
+    );
+  }
+  
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,38 @@
 import Image from "next/image";
-import ggg from "./signin.png"
+import figma from "./images/signin.png";
+import {AiFillGithub} from "react-icons/ai"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <p className="m-40 text-center text-xl">
-        Under Maintenance
-        <br />
-        You can view the project on figma:
-        <br/>
-        <a href="https://www.figma.com/file/fUAeNhJwbc2ka73nfguHLy/Your-Fitness-Journey?type=design&node-id=0%3A1&mode=design&t=OiXPJJupINZfrXei-1">
-      <Image
-      className="m-5"
-        src={ggg}
-        alt="figma file"
-        width={350}
-        height={150}
-      ></Image></a>
-      </p>
+    <main className="flex flex-col cursor-default h-screen">
+      <section className="flex justify-center flex-col py-4 px-6 mx-auto mt-10 rounded-3xl border-4 border-slate-600 bg-white opacity-90">
+        <div className="text-center">
+          <p className="text-2xl underline pb-6">Under Maintenace</p>
+          <p className=" text-xl">Project can be viewed at:</p>
+        </div>
+        <div className="flex flex-row pt-8 pb-4 pr-10 gap-9">
+        <div className="flex flex-col text-center">
+          <h1 className="text-xl pb-2 underline">Figma</h1>
+          <a href="https://www.figma.com/file/fUAeNhJwbc2ka73nfguHLy/Your-Fitness-Journey?type=design&node-id=0%3A1&mode=design&t=OiXPJJupINZfrXei-1">
+            <Image
+              className="m-5"
+              src={figma}
+              alt="figma file"
+              width={250}
+              height={150}
+            ></Image>
+          </a>
+          {/* bg-gradient-to-b from-slate-500 */}
+        </div>
+        <div className="flex flex-col text-center">
+          <h1 className="text-xl pb-4 underline">GitHub</h1>
+          <a href="https://github.com/ChiaBeaCode/BuildingYourFitnessJourney">
+            <AiFillGithub className=" text-8xl"/>
+          </a>
+          {/* bg-gradient-to-b from-slate-500 */}
+        </div>
+        </div>
+      </section>
     </main>
   );
 }
